4af33f369d05eb361ab12935e47aeefed9d528df
Lev Yuvenskiy <yuvenskiylev@mail.ru>
2025-05-19 19:54:56 +0300

    fix close buffers

--- lua/Yu-Leo/buffers/init.lua
+++ lua/Yu-Leo/buffers/init.lua
@@ -10,7 +10,14 @@ M.close_all_hidden = function()
 
   for _, buf in ipairs(vim.api.nvim_list_bufs()) do
     local buftype = vim.api.nvim_get_option_value("buftype", { buf = buf })
-    if not open_bufs[buf] and buftype ~= "quickfix" and buftype ~= "nofile" then
+    if
+      not open_bufs[buf]
+      and buftype ~= "quickfix"
+      and buftype ~= "nofile"
+      and buftype ~= "neotest-output"
+      and buftype ~= "neotest-summary"
+      and buftype ~= "terminal"
+    then
       pcall(vim.cmd, "confirm bdelete " .. buf)
     end
   end
