5d4b2f487351e724088109191dc0accbd0b58069
Lev Yuvenskiy <yuvenskiylev@mail.ru>
2025-05-22 23:09:36 +0300

    migrate to 0.11 WIP

---
+++ lua/lsp/init.lua
@@ -0,0 +1,67 @@
+local utils = require "lsp.utils"
+
+vim.diagnostic.config {
+  virtual_lines = false,
+  virtual_text = {
+    prefix = "",
+  },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.INFO] = "",
+      [vim.diagnostic.severity.HINT] = "",
+    },
+  },
+  underline = true,
+  float = {
+    border = "single",
+  },
+  severity_sort = true,
+}
+
+utils.remove_default_mappings()
+
+vim.lsp.config("gopls", {
+  cmd = { "gopls", "serve" },
+  filetypes = { "go", "go.mod" },
+  root_markers = { "go.work", "go.mod", ".git" },
+  on_init = utils.on_init,
+  on_attach = utils.on_attach,
+})
+
+vim.lsp.config("lua_ls", {
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  root_markers = { ".git", ".luarc.json", ".luarc.jsonc" },
+  on_init = utils.on_init,
+  on_attach = utils.on_attach,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          vim.fn.expand "$VIMRUNTIME/lua",
+          vim.fn.expand "$VIMRUNTIME/lua/vim/lsp",
+          vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
+          vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
+          "${3rd}/luv/library",
+        },
+        maxPreload = 100000,
+        preloadFileSize = 10000,
+      },
+    },
+  },
+})
+
+local brief_exists, brief = pcall(require, "private.brief")
+if brief_exists then
+  brief.setup_briefls(utils.on_attach, utils.on_init)
+end
+
+vim.lsp.enable {
+  "gopls",
+  "lua_ls",
+}
---
+++ lua/lsp/utils.lua
@@ -0,0 +1,60 @@
+local M = {}
+
+local map = vim.keymap.set
+
+M.on_init = function(client, _)
+  if client.supports_method "textDocument/semanticTokens" then
+    client.server_capabilities.semanticTokensProvider = nil
+  end
+end
+
+M.on_attach = function(client, bufnr)
+  local function map_opts(desc)
+    return { buffer = bufnr, desc = "LSP: " .. desc }
+  end
+
+  local floating_window_opts = {
+    border = "single",
+    max_width = 80,
+  }
+
+  map("n", "gD", vim.lsp.buf.declaration, map_opts "go to declaration")
+  map("n", "gd", vim.lsp.buf.definition, map_opts "go to definition")
+  map("n", "gi", vim.lsp.buf.implementation, map_opts "go to implementation")
+  map("n", "gj", vim.lsp.buf.type_definition, map_opts "go to type definition")
+
+  map("n", "gk", function()
+    require("lsp.handlers").references { includeDeclaration = false }
+  end, map_opts "show references")
+
+  map("n", "K", function()
+    return vim.lsp.buf.hover(floating_window_opts)
+  end, map_opts "hover")
+
+  map("n", "<leader>ld", function()
+    return vim.diagnostic.open_float(floating_window_opts)
+  end, map_opts "diagnostic open float")
+
+  map("n", "<leader>ls", function()
+    return vim.lsp.buf.signature_help(floating_window_opts)
+  end, map_opts "show signature help")
+
+  map("i", "<c-s>", function()
+    return vim.lsp.buf.signature_help(floating_window_opts)
+  end, map_opts "show signature help")
+
+  map("n", "<leader>lr", vim.lsp.buf.rename, map_opts "rename")
+
+  map({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, map_opts "code action")
+  map({ "n", "v" }, "<leader>ll", vim.lsp.codelens.run, map_opts "code lens")
+end
+
+M.remove_default_mappings = function()
+  vim.keymap.del("n", "grn")
+  vim.keymap.del("n", "gra")
+  vim.keymap.del("n", "grr")
+  vim.keymap.del("n", "gri")
+  vim.keymap.del("n", "gO")
+end
+
+return M
